4de62ab009437048b2f0e23ffb08659eba043930
Ashwin Ramesh <ashwinramesh92@hotmail.com>
2013-05-01 22:06:30 +1000

    i think i finished bayes. havnt tested

--- naiveBayes.py
+++ naiveBayes.py
@@ -13,8 +13,6 @@ def convert_array_to_dict(inputArray,headers):
 	outputArray = {}
 	for header in headers[:-1]: # don't include class
 		outputArray[header] = inputArray[headers.index(header)]
-	#print inputArray
-	#print outputArray
 	return outputArray
 
 # Calculate the Mean and Standard Dev. for all columns. Returns array[array[mean,sd]*7] for each column.
@@ -66,28 +64,40 @@ def calculate_mean_sd(inputData):
 	return class_zero,class_one;
 
 # Calculate the PDF value for a given mean and SD
-def calculatePDF(value,colMean,colSD):
-	return 1
-
-# Probability of YES|NO for diabetes overall from dataset
-def calculateProbabilities(data):
-	return 1
-
-def initBayes(inputData):
-	prob = calculateProbabilities(1)
-	meanSD_array = calculate_mean_sd(1)
-	return 1
+def calculate_PDF(inputArray,class_zero,class_one):
+	pdf_base_val = math.exp(1)/math.sqrt(1/(2*math.pi))
+	headers = data_preprocessing.get_header()
+	pdf_array = {'class_zero':{},'class_one':{}}
+	for header in headers[:-1]:
+		pdf_array['class_zero'][header] = math.pow((pdf_base_val / class_zero[header]['sd']),(-(math.pow(inputArray[header]-class_zero[header]['mean'],2))/(2*math.pow(class_zero[header]['sd'],2))))
+		pdf_array['class_one'][header] = math.pow((pdf_base_val / class_one[header]['sd']),(-(math.pow(inputArray[header]-class_one[header]['mean'],2))/(2*math.pow(class_one[header]['sd'],2))))
+	return pdf_array
 
 # Classify a given dataSet. Requires the output of calculateMeanAndSD.
-def classify(dataSet,meanSD_array):
-	return 1
+def classify(inputArray,class_zero,class_one):
+	headers = data_preprocessing.get_header()
+	inputArray = convert_array_to_dict(inputArray, headers)
+	pdf_array = calculate_PDF(inputArray,class_zero,class_one)
+	test_one_val = class_one['size']/(class_one['size']+class_zero['size']) # total percentage of classOne
+	test_zero_val =  class_zero['size']/(class_one['size']+class_zero['size']) # total percentage of classZero
+	for header in headers[:-1]: # multiplying out the bayes value for 0 and 1
+		test_one_val = test_one_val * pdf_array['class_one'][header]
+		test_zero_val = test_zero_val * pdf_array['class_zero'][header]
+	if ((test_one_val - test_zero_val) >= 0):
+		return 1 # for Diabetic
+	else:
+		return 0 # for Non-Diabetic
+
+def init_bayes(file_name):
+	training_data = data_preprocessing.load_csv_data(file_name)
+	training_data.pop(0) # remove top row
+	return calculate_mean_sd(training_data)
 
 def main():
-	training_data = data_preprocessing.load_csv_data("pima.csv")
-	training_data.pop(0) # remove top row
-	(class_zero, class_one) = calculate_mean_sd(training_data)
+	(class_zero, class_one) = init_bayes("pima.csv")
 	print "class0: " + str(class_zero)
 	print "class1:  " + str(class_one)
+
 	#initBayes(training_data)
 	return 1;
 
