0b3d4cb23c08cb7db499be140d9ba317a44b45f7
Ashwin Ramesh <ashwinramesh92@hotmail.com>
2013-05-04 19:22:28 +1000

    fixed naive. works with weka output

--- naiveBayes.py
+++ naiveBayes.py
@@ -2,11 +2,12 @@
 # Assignment 1
 # @author Ashwin Ramesh
 # SID 311254012
-
+from __future__ import division
 import data_preprocessing
 import math
 from heapq import heappush, heappop
 import random
+
 
 # convert a array of data into a dictionary of headers=>data
 def convert_array_to_dict(inputArray,headers):
@@ -14,6 +15,14 @@ def convert_array_to_dict(inputArray,headers):
 	for header in headers[:-1]: # don't include class
 		outputArray[header] = inputArray[headers.index(header)]
 	return outputArray
+
+#Formated Printing of Mean and SD
+def print_mean_sd(data,name):
+	print_line = "%s"%(name)
+	for key in data.keys():
+		temp = "\n %s: %s" %(key,str(data[key]))
+		print_line = print_line + temp
+	return print_line
 
 # Calculate the Mean and Standard Dev. for all columns. Returns array[array[mean,sd]*7] for each column.
 def calculate_mean_sd(inputData):
@@ -63,14 +72,19 @@ def calculate_mean_sd(inputData):
 
 	return class_zero,class_one;
 
+def PDF_math(attr_val,mean,sd):
+	fraction_val = (1/sd) * math.sqrt(1/(2*math.pi))
+	power_val = (-1) * (math.pow((attr_val-mean),2)) / (2*math.pow(sd,2))
+	value = fraction_val * math.exp(power_val)
+	return value
+
 # Calculate the PDF value for a given mean and SD
 def calculate_PDF(inputArray,class_zero,class_one):
-	pdf_base_val = math.exp(1)/math.sqrt(1/(2*math.pi))
 	headers = data_preprocessing.get_header()
 	pdf_array = {'class_zero':{},'class_one':{}}
 	for header in headers[:-1]:
-		pdf_array['class_zero'][header] = math.pow((pdf_base_val / class_zero[header]['sd']),(-(math.pow(inputArray[header]-class_zero[header]['mean'],2))/(2*math.pow(class_zero[header]['sd'],2))))
-		pdf_array['class_one'][header] = math.pow((pdf_base_val / class_one[header]['sd']),(-(math.pow(inputArray[header]-class_one[header]['mean'],2))/(2*math.pow(class_one[header]['sd'],2))))
+		pdf_array['class_zero'][header] = PDF_math(inputArray[header],class_zero[header]['mean'],class_zero[header]['sd'])
+		pdf_array['class_one'][header] = PDF_math(inputArray[header],class_one[header]['mean'],class_one[header]['sd'])
 	return pdf_array
 
 # Classify a given dataSet. Requires the output of calculateMeanAndSD.
@@ -78,8 +92,8 @@ def classify(inputArray,class_zero,class_one):
 	headers = data_preprocessing.get_header()
 	inputArray = convert_array_to_dict(inputArray, headers)
 	pdf_array = calculate_PDF(inputArray,class_zero,class_one)
-	test_one_val = class_one['size']/(class_one['size']+class_zero['size']) # total percentage of classOne
-	test_zero_val =  class_zero['size']/(class_one['size']+class_zero['size']) # total percentage of classZero
+	test_one_val = float(class_one['size'])/float(class_one['size']+class_zero['size']) # total percentage of classOne
+	test_zero_val =  float(class_zero['size'])/float(class_one['size']+class_zero['size']) # total percentage of classZero
 	for header in headers[:-1]: # multiplying out the bayes value for 0 and 1
 		test_one_val = test_one_val * pdf_array['class_one'][header]
 		test_zero_val = test_zero_val * pdf_array['class_zero'][header]
@@ -95,8 +109,14 @@ def init_bayes(file_name):
 
 def main():
 	(class_zero, class_one) = init_bayes("pima.csv")
-	print "class0: " + str(class_zero)
-	print "class1:  " + str(class_one)
+	data = data_preprocessing.load_csv_data("pima.csv")
+	data.pop(0)
+	print classify(data[0], class_zero, class_one)
+	print classify(data[1], class_zero, class_one)
+	print classify(data[2], class_zero, class_one)
+	print print_mean_sd(class_zero,"Class Zero")
+	print print_mean_sd(class_one, "Class One")
+
 
 	#initBayes(training_data)
 	return 1;
