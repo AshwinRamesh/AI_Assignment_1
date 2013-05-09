812dbd31e14fa25a3a8ae7a4b454d47d9ab6c5a9
Ashwin Ramesh <ashwinramesh92@hotmail.com>
2013-05-09 21:44:48 +1000

    fixed naive

--- naiveBayes.py
+++ naiveBayes.py
@@ -5,15 +5,21 @@
 from __future__ import division
 import data_preprocessing
 import math
-from heapq import heappush, heappop
 import random
 
 
-# convert a array of data into a dictionary of headers=>data
-def convert_array_to_dict(inputArray,headers):
+# convert a array of data into a dictionary of headers=>data Custom Headers is a subset of all the headers. It will return an associative array containing only those headers
+def convert_array_to_dict(inputArray,custom_headers=False):
+	headers = data_preprocessing.get_header()
 	outputArray = {}
 	for header in headers: # don't include class
 		outputArray[header] = inputArray[headers.index(header)]
+	if (custom_headers != False): # subset of headers given
+		temp_output = {}
+		for key in custom_headers:
+			if key in outputArray:
+				temp_output[key] = outputArray[key]
+		outputArray = temp_output
 	return outputArray
 
 #Formated Printing of Mean and SD
@@ -43,11 +49,8 @@ def calculate_mean_sd(inputData,attr_names=False):
 
 	# Calculate Mean #
 	for row in inputData:
-		print "INPUT: "+ str(row)
-		dictRow = convert_array_to_dict(row,headers)
-		print "ROWS: " +  str(dictRow)
+		dictRow = convert_array_to_dict(row,attr_names)
 		class_name = dictRow.pop("class")
-		print "ROW: "+ str(class_name)
 		if class_name == "class1": # for class_one
 			for key in dictRow.keys():
 				class_one[key]['mean'] += dictRow[key]
@@ -63,7 +66,7 @@ def calculate_mean_sd(inputData,attr_names=False):
 
 	# Calculate SD
 	for row in inputData:
-		dictRow = convert_array_to_dict(row, headers)
+		dictRow = convert_array_to_dict(row, attr_names)
 		class_name = dictRow.pop("class")
 		if class_name == "class1": # for class_one
 			for key in dictRow.keys():
@@ -102,14 +105,14 @@ def classify(inputArray,class_zero,class_one,attr_names=False):
 		headers = data_preprocessing.get_header()
 	else:
 		headers = attr_names
-	inputArray = convert_array_to_dict(inputArray, headers)
+	inputArray = convert_array_to_dict(inputArray, attr_names)
 	pdf_array = calculate_PDF(inputArray,class_zero,class_one,attr_names)
 	test_one_val = float(class_one['size'])/float(class_one['size']+class_zero['size']) # total percentage of classOne
 	test_zero_val =  float(class_zero['size'])/float(class_one['size']+class_zero['size']) # total percentage of classZero
 	for header in headers[:-1]: # multiplying out the bayes value for 0 and 1
 		test_one_val = test_one_val * pdf_array['class_one'][header]
 		test_zero_val = test_zero_val * pdf_array['class_zero'][header]
-	print "one: %f zero: %f "%(test_one_val,test_zero_val)
+	#print "one: %f zero: %f "%(test_one_val,test_zero_val)
 	if ((test_one_val - test_zero_val) >= 0):
 		if inputArray['class'] == 'class1': # return True if actual == calculated
 			return 1,True # for Diabetic
@@ -125,16 +128,23 @@ def init_bayes(file_name,attr_names=False):
 
 def main():
 	attr_names = ['plasma_glucose_concentration','bmi','diabetes_pedigree','age','class'] # For CFS
-	(class_zero, class_one) = init_bayes("pima.csv",attr_names)
+	(class_zero, class_one) = init_bayes("pima.csv")
+	#print print_mean_sd(class_zero,"Class Zero")
+	#print print_mean_sd(class_one, "Class One")
+
 	data = data_preprocessing.load_csv_data("pima.csv")
 	data.pop(0)
+	count_correct = 0
+	count_incorrect = 0
 	for item in data:
-		print classify(item, class_zero, class_one,attr_names)
-	#print classify(data[0], class_zero, class_one,attr_names)
-	#print classify(data[1], class_zero, class_one)
-	#print classify(data[2], class_zero, class_one,attr_names)
-	print print_mean_sd(class_zero,"Class Zero")
-	print print_mean_sd(class_one, "Class One")
+		(a,out) =  classify(item, class_zero, class_one)
+		if out == True:
+			count_correct = count_correct + 1
+		else:
+			count_incorrect = count_incorrect + 1
+	print "Correct: %d    Incorrect: %d" %(count_correct,count_incorrect)
+	#print print_mean_sd(class_zero,"Class Zero")
+	#print print_mean_sd(class_one, "Class One")
 
 
 	#initBayes(training_data)
